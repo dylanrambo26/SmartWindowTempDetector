04f7570f0f876acf5adf61ea4d32dd5b8beaa547
dylanrambo26 <145638883+dylanrambo26@users.noreply.github.com>
2025-11-20 15:35:31 -0800

    Added temp checking logic

--- main.py
+++ main.py
@@ -5,18 +5,31 @@ import textBeltAPI
 
 def main():
     print("Starting main logic...")
-    textBeltAPI.open_window(70)
+    # textBeltAPI.open_window(70)
 
-    
+    #Start at default state
+    lowTriggeredOnce = False 
+    highTriggeredOnce = False
 
     while True:
+        #currentTemp = TEMP_FROM_SENSOR_HERE
+
         config = SmartWindow2.load_config()
         print("Current config:", config)
 
         open_temp = config["open_temp"]
         close_temp = config["close_temp"]
 
-
+        if not lowTriggeredOnce:
+            if currentTemp < open_temp:
+                textBeltAPI.open_window(open_temp)
+                lowTriggeredOnce = True
+                selfhighTriggeredOnce = False
+        if not highTriggeredOnce:
+            if currentTemp > close_temp:
+                textBeltAPI.close_window(close_temp)
+                highTriggeredOnce = True
+                lowTriggeredOnce = False
 
         # Example placeholder logic
         # temp = temp_sensor.read_fahrenheit()
@@ -26,7 +39,7 @@ def main():
         #     close_window()
 
         time.sleep(5)
-
+    
 #Start the web server in another thread
 _thread.start_new_thread(SmartWindow2.start_server, ())
 
